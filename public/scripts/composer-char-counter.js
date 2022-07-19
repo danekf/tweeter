2108ea03566fcdf384828c29eb22588a2a6790ae
Danek <danek.fill@outlook.com>
2022-07-19 19:15:44 +0000

    Counter implemented, turns red if exceeds 140 characters

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,39 @@
+$(document).ready(() => {
+
+//on type, update remaining char count
+$("#tweet-text").on('input', function () {
+
+  let text = $(this).val();
+  console.log(text);
+  length = 140-text.length; 
+
+  //target the counter element inside the new-tweet parent
+  let counter = $('.new-tweet').find('output[name="counter"]');
+
+  //change class and colour if negative, return to normal if not
+  if(length < 0){
+    $(counter).removeClass("counter");
+    $(counter).addClass("negative");
+    
+
+  } else{
+    $(counter).removeClass("negative");
+    $(counter).addClass("counter");
+
+  };
+  $(counter).text(length);
+
+
+
+
+
+
+
+
+
+});
+
+
+
+});
+
